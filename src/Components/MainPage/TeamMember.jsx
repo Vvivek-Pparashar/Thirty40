79fa398f8bc0f65815407d1bdc985ca3814d8334
Vivek Parashar <96679102+Vvivek-Pparashar@users.noreply.github.com>
2024-04-19 23:55:48 +0530

    updated

--- src/Components/MainPage/TeamMember.jsx
+++ src/Components/MainPage/TeamMember.jsx
@@ -1,59 +1,60 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import OwlCarousel from 'react-owl-carousel';
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';
+import arnaavProfile from '../../../public/img/owner.jpg'
+import './TeamMember.css'
 
 const TeamMember = () => {
   return (
     <div>
-      <div className="container-fluid py-5">
-        <div className="container">
-          <div
-            className="border-start border-5 border-primary ps-5 mb-5"
-            style={{ maxWidth: 600 + "px" }}
-          >
-            <h6 className="text-primary text-uppercase">Team Members</h6>
-            <h1 className="display-5 text-uppercase mb-0">
-              Qualified Pets Care Professionals
-            </h1>
-          </div>
+      <div>
+        <div className="container-fluid py-5">
+          <div className="container">
+            <div
+              className="border-start border-5 border-primary ps-5 mb-5"
+              style={{ maxWidth: 600 + "px" }}
+            >
+              <h6 className="text-primary text-uppercase">The Owner</h6>
+              <h1 className="display-5 text-uppercase mb-0">
+                The Owner
+              </h1>
+            </div>
 
-          <OwlCarousel
-            className="owl-carousel team-carousel position-relative"
-            style={{ paddingRight: 25 + "px" }}
-          >
-            <div className="team-item">
-              <div className="position-relative overflow-hidden">
-                <img className="img-fluid w-100" src="img/team-1.jpg" alt="" />
-                <div className="team-overlay">
-                  <div className="d-flex align-items-center justify-content-start">
-                    <Link className="btn btn-light btn-square mx-1" href="#">
-                      <i className="bi bi-twitter"></i>
-                    </Link>
-                    <Link className="btn btn-light btn-square mx-1" href="#">
-                      <i className="bi bi-facebook"></i>
-                    </Link>
-                    <Link className="btn btn-light btn-square mx-1" href="#">
-                      <i className="bi bi-linkedin"></i>
-                    </Link>
+            <div className="try text-center p-4 px-5 d-flex justify-between-center align-items-center">
+              <div className="card">
+                <div className="img">
+                  <img src={arnaavProfile} />
+                </div>
+                <div className="infos">
+                  <div className="name">
+                    <h2>ARNAV ALOK GOEL</h2>
+                    <h4>@_thirty40</h4>
                   </div>
+                  <p className="text">
+                    Coached various ATP & WTA players, Hitting partner of World
+                    No. 1 - Karolina Pliskova
+                  </p>
+                  <ul className="stats">
+                    <li>
+                      <h3>2188</h3>
+                      <h4>Highest ATP Rank </h4>
+                    </li>
+                    <li>
+                      <h3>1377</h3>
+                      <h4>Highest ITF Rank</h4>
+                    </li>
+                    <li>
+                      <h3>42</h3>
+                      <h4>AITA Rank</h4>
+                    </li>
+                    <li>
+                      <h3>1/6</h3>
+                      <h4>German Rank 449 French Rank</h4>
+                    </li>
+                  </ul>
                 </div>
               </div>
-              <div className="bg-light text-center p-4">
-                <h5 className="text-uppercase">ARNAV ALOK GOEL</h5>
-                <p className="m-0">
-                  Coached various ATP & WTA players Hitting partner of World No.
-                  1 - Karolina Pliskova <br />
-                  Highest ATP Rank 2188 <br />
-                  Highest ITF Rank 1377 <br />
-                  AITA Rank 42 <br />
-                  German Rank 449 <br />
-                  French Rank 1/6
-                </p>
-              </div>
+              <div className="owner-img"></div>
             </div>
-          </OwlCarousel>
+          </div>
         </div>
       </div>
     </div>
